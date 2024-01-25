24b9a0b93afdd8d296cc88b0b7e716fd924c0a91
Malcolm Kam <malcolm.h.kam@gmail.com>
2024-01-25 13:52:49 -1000

    Implemented linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,32 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    if (list.tail === null) {
+      list.tail = Node(value);
+      list.head = list.tail;
+    }
+    list.tail = Node(value);
+    list.head.next = list.tail;
+
+
+
   };
 
   list.removeHead = function() {
+    var result = list.head.value;
+    list.head = list.tail;
+    return result;
+    // var result = value;
+    // delete value;
+    // return result;
   };
 
   list.contains = function(target) {
+    // for (var key in list) {
+    //   return list[key]['value'] === target;
+    // }
+
+    return (list.head.value === target || list.tail.value === target);
   };
 
   return list;
@@ -23,7 +43,3 @@ var Node = function(value) {
 
   return node;
 };
-
-/*
- * Complexity: What is the time complexity of the above functions?
- */
