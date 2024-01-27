a038f71ea77e83f1644f339d6d442e6592649818
Malcolm Kam <malcolm.h.kam@gmail.com>
2024-01-27 08:57:31 -1000

    Edited hashTable with advanced content to double and halve size

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,6 +1,8 @@
+
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._tupleCount = 0;
 };
 
 HashTable.prototype.insert = function(k, v) {
@@ -24,11 +26,60 @@ HashTable.prototype.insert = function(k, v) {
 
   // If key doesn't exist in hash table, push key-value pair to bucket
   bucket.push([k, v]);
+  this._tupleCount++;
 
-  // Check if index within this._storage array is undefined
-  // Manually create array and nest the key-value pair within the array we created
-  // If index is not undefined
-  // Push key-value pair to array within that index
+  // Checking ratio of tupleCount to limit
+  // If tupleCount > 75% of limit
+  if ((this._limit * 0.75) < this._tupleCount) {
+    // Double limit
+    this.resize(this._limit * 2);
+  }
+};
+
+HashTable.prototype.resize = function(newLimit) {
+  var oldStorage = this._storage;
+
+  this._limit = newLimit;
+  this._storage = LimitedArray(this._limit);
+  this._tupleCount = 0;
+
+  // Iterate through oldStorage and move tuples to new storage buckets
+  oldStorage.each(function(bucket) {
+    // FInd a bucket
+    if (bucket) {
+      for (var i = 0; i < bucket.length; i++) {
+        // Take the tuple(s) in that bucket and insert them into new storage array
+        var tuple = bucket[i];
+        this.newInsert(tuple[0], tuple[1]);
+      }
+    }
+  }.bind(this));
+
+
+};
+
+HashTable.prototype.newInsert = function(k, v) {
+  var index = getIndexBelowMaxForKey(k, this._limit);
+  // Call the set function within LimitedArray to set value to index
+  // Create array within array to handle collisions
+  if (this._storage.get(index) === undefined) {
+    this._storage.set(index, []);
+  }
+  // Set bucket to equal current index
+  var bucket = this._storage.get(index);
+
+  // Iterate over bucket length if key already exists in hash table
+  for (var i = 0; i < bucket.length; i++) {
+    var tuple = bucket[i];
+    if (tuple[0] === k) {
+      tuple[1] = v;
+      return;
+    }
+  }
+
+  // If key doesn't exist in hash table, push key-value pair to bucket
+  bucket.push([k, v]);
+  this._tupleCount++;
 };
 
 HashTable.prototype.retrieve = function(k) {
@@ -43,7 +94,6 @@ HashTable.prototype.retrieve = function(k) {
       }
     }
   }
-  //return this._storage.get(index);
 };
 
 HashTable.prototype.remove = function(k) {
@@ -54,9 +104,24 @@ HashTable.prototype.remove = function(k) {
     for (var i = 0; i < bucket.length; i++) {
       var tuple = bucket[i];
       if (tuple[0] === k) {
-        tuple[1] = undefined;
+        bucket.splice(tuple, 1);
+        this._tupleCount--;
       }
     }
   }
-  // return this._storage.set(index, undefined);
+
+
+  // If tupleCount < 25% of limit
+  if (this._tupleCount < this._limit * 0.25) {
+    // Halve limit
+    this.resize(this._limit / 2);
+  }
 };
+
+
+
+/*
+ * Complexity: What is the time complexity of the above functions?
+ */
+
+
