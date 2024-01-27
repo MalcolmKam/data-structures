a28358d9e83c560aa001d1a312b74f8a574d3ee6
Malcolm Kam <malcolm.h.kam@gmail.com>
2024-01-27 10:32:48 -1000

    converted linkedList to a doubly linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,35 +1,82 @@
+//technically doubly linked but I didn't create another section for that
+
 var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
     if (list.tail === null) {
-      list.tail = Node(value);
-      list.head = list.tail;
+      list.tail = newNode;
+      list.head = newNode;
+    } else {
+      newNode.previous = list.tail;
+      list.tail.next = newNode;
+      list.tail = newNode;
     }
-    list.tail = Node(value);
-    list.head.next = list.tail;
 
-
-
+    // list.head.next = list.tail;
+    //constant
   };
 
   list.removeHead = function() {
+    if (list.head === null) {
+      return null;
+    }
     var result = list.head.value;
-    list.head = list.tail;
+    // delete list.head;
+
+
+    if (list.head.next !== null) {
+      list.head.next.previous = null;
+    }
+
+    list.head = list.head.next;
+
     return result;
-    // var result = value;
-    // delete value;
-    // return result;
   };
 
   list.contains = function(target) {
-    // for (var key in list) {
-    //   return list[key]['value'] === target;
-    // }
+    var currentNode = list.head;
 
-    return (list.head.value === target || list.tail.value === target);
+    while (currentNode !== null) {
+      if (currentNode.value === target) {
+        return true;
+      }
+      currentNode = currentNode.next;
+    }
+    return false;
+    // return (list.head.value === target || list.tail.value === target);
+  };
+
+  list.addToHead = function(value) {
+    var newNode = Node(value);
+    if (list.head === null) {
+      list.tail = newNode;
+      list.head = newNode;
+    } else {
+      newNode.next = list.head;
+      list.head.previous = newNode;
+      list.head = newNode;
+    }
+  };
+
+  list.removeTail = function() {
+    if (list.tail === null) {
+      return null;
+    }
+    var result = list.tail.value;
+    // delete list.head;
+
+
+    if (list.tail.previous !== null) {
+      list.tail.previous.next = null;
+    }
+
+    list.tail = list.tail.previous;
+
+    return result;
   };
 
   return list;
@@ -40,6 +87,7 @@ var Node = function(value) {
 
   node.value = value;
   node.next = null;
+  node.previous = null;
 
   return node;
 };
